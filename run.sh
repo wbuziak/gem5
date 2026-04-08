d1232577ca02d68d4ac54b6c7b594b7810574c4a
wbuziak <willbuziak@gmail.com>
2026-04-08 18:58:13 +0000

    run.sh save

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 export CUR_DIR=$(pwd)
-export BMK=$1
-export SIZE=$2
-export MCX=$3
-export META_SIZE=$4
-export LLC_SIZE=$5
+export BMK=$1 # benchmark
+export SIZE=$2 # simulation size (simlarge, simsmall)
+export META_SIZE=$3 # metadata cache size
+export CONFIG=$4 # architecture / name / mem-module we are testing (config file)
 
-RESULTS_DIR=$CUR_DIR/results/$BMK-$SIZE/$MCX-$META_SIZE-$LLC_SIZE
+# redirect the stats to a folder with the following naming convention
+RESULTS_DIR=$CUR_DIR/results/$CONFIG/$BMK-$SIZE/$META_SIZE
 
-$CUR_DIR/build/X86/gem5.opt -d $RESULTS_DIR $CUR_DIR/configs/example/gem5_library/x86-parsec-mcx_v3.py --benchmark $BMK --size $SIZE --metadata_cache_size $META_SIZE --l3_size $LLC_SIZE --mcx_policy $MCX --cache_mac
+$CUR_DIR/build/X86/gem5.opt -d $RESULTS_DIR $CUR_DIR/configs/example/gem5_library/$CONFIG --benchmark $BMK --size $SIZE --metadata_cache_size $META_SIZE --cache_mac
 
-
+# save the config file
+cp $CUR_DIR/configs/example/gem5_library/$CONFIG $RESULTS_DIR/
