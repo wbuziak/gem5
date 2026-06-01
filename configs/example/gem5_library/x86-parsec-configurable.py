4dc4b54d162e92f2690a6b57f81adeabd0c9c2c9
wbuziak <willbuziak@gmail.com>
2026-06-01 22:05:36 +0000

    config

--- configs/example/gem5_library/x86-parsec-configurable.py
+++ configs/example/gem5_library/x86-parsec-configurable.py
@@ -55,7 +55,7 @@ from gem5.coherence_protocol import CoherenceProtocol
 from gem5.components.boards.x86_board import X86Board
 from gem5.components.memory.secure_ddr4 import ConfigurableMemory 
 from gem5.components.memory.secure_ddr4 import IntegrityTreeProtectedMemory
-from gem5.components.memory.multi_channel import DualChannelDDR3_1600
+from gem5.components.memory.multi_channel import DualChannelDDR4_2400
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.components.processors.simple_switchable_processor import (
     SimpleSwitchableProcessor,
@@ -270,7 +270,7 @@ elif (args.memory == "mcx"):
         protocol=args.mcx_policy,
     )
 else:
-    memory = DualChannelDDR3_1600(
+    memory =DualChannelDDR4_2400( 
         size="16GiB",
     )
 
@@ -390,8 +390,6 @@ simulator.run()
 print("All simulation events were successful.")
 
 # We print the final simulation statistics.
-
-print("Done with the simulation")
 print()
 print("Performance statistics:")
 
